9a8cc3d89d7ebbb24314abab553e4f36c2738ae7
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-22 11:35:08 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -20,7 +20,7 @@ def print_first_directors_movie_titles
   end
   increment = 0
   while increment < directors.count do
-    movie_title = directors[increment].each {| i | print i[:title]}
+    movie_title = directors[increment].each {| i | p i[:title]}
     increment+= 1
   end
   # p movie_title
