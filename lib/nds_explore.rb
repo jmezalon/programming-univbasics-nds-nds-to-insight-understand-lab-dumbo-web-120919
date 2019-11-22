dd3ea128c0e371960d1821d20d081c8570b00eba
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-22 11:32:56 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,8 +10,8 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  directors = []
-  movie_title = {}
+  directors = {}
+  movie_title = []
   
   count = 0
   while count < directors_database.length do
@@ -20,7 +20,7 @@ def print_first_directors_movie_titles
   end
   increment = 0
   while increment < directors.count do
-    movie_title[increment] = directors[increment]
+    movie_title = directors[increment][:title]
     increment+= 1
   end
   p movie_title
