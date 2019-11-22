98e9ada7f7743213a0eed5499074dad7731bc07e
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-22 11:19:58 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -18,7 +18,7 @@ def print_first_directors_movie_titles
     directors << directors_database[count][:movies]
     count+= 1
   end
-  directors.each { | title | print title }
+  directors.each { | title | print title.title }
 end
 
 
